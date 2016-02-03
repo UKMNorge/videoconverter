c095e023ca9227af5a7a5fb8087461afb70d9a09
Marius Mandal <mariusmandal@gmail.com>
2016-02-03 23:46:08 +0100

    Logg SQL-spørringer

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,5 +1,7 @@
 <?php
 function ukmtv_update($field, $status, $id) {	
+	logg('DB: '. "UPDATE `ukmtv` SET `".$field."` = '".$status."' WHERE `id` = '".$id."'");
+	
 	mysql_query("UPDATE `ukmtv` SET `".$field."` = '".$status."' WHERE `id` = '".$id."'") or die(mysql_error());
 }
 
