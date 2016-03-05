6980a48882f5a7445db1d0e132a7227f75de9ea3
Marius Mandal <mariusmandal@gmail.com>
2016-03-05 11:08:54 +0100

    tidsberegning

--- api/status.php
+++ api/status.php
@@ -22,6 +22,7 @@ $sql = "SELECT COUNT(`id`) AS `count`
 		FROM `ukmtv`
 		WHERE `status_progress` = 'registered'";
 $info->queue->first_convert = queue_count( $sql );
+$info->time->first_convert = $info->queue->first_convert * 5; // Estimated 5 min
 
 // ANTALL FINAL-CONVERT I KØ
 $sql = "SELECT COUNT(`id`) AS `count`
@@ -29,6 +30,7 @@ $sql = "SELECT COUNT(`id`) AS `count`
 		WHERE `status_progress` = 'converting'
 		AND `status_first_convert` = 'complete'";
 $info->queue->final_convert = queue_count( $sql );
+$info->time->final_convert = $info->queue->first_convert * 10; // Estimated 10 min
 
 // ANTALL ARCHIVE-CONVERT I KØ
 $sql = "SELECT COUNT(`id`) AS `count`
@@ -36,5 +38,8 @@ $sql = "SELECT COUNT(`id`) AS `count`
 		WHERE `status_progress` = 'archive'
 		AND `status_final_convert` = 'complete'";
 $info->queue->archive_convert = queue_count( $sql );
+$info->time->archive_convert = $info->queue->first_convert * 25; // Estimated 25 min
+
+$info->time->total = (int)$info->time->archive_convert + (int)$info->time->final_convert + (int)$info->time->first_convert;
 
 die(json_encode($info));
