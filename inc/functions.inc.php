0b9e56c4f0c97d7b992308d90015c26a06a2c338
Marius Mandal <mariusmandal@gmail.com>
2016-04-18 14:46:21 +0200

    Logger til fil slik at UKMWPadmin kan lese ut videoconverter-status

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -6,9 +6,18 @@ function ukmtv_update($field, $status, $id) {
 }
 
 function logg( $message ) {
+	$file_name = DIR_LOG . 'cron_'. CRON_ID .'.log';
+	$file_handle = fopen( $file_name, 'a');
+	fwrite( $file_handle, "\r\n" . date('d.m H:i') .': '. $message );
+	fclose( $file_handle );
     error_log(LOG_SCRIPT_NAME .' '. CRON_ID .': '. $message );
-}
+} 
 
 function notify( $message ) {
+	logg( $message );
+	if( defined('CRON_ID') && is_numeric( CRON_ID ) ) {
+		mysql_query("UPDATE `ukmtv` SET `admin_notice` = 'true' WHERE `id` = '". CRON_ID ."'") or die(mysql_error());
+	}
+    
     error_log('SERVER ADMIN NOTIFICATION: cid'. CRON_ID .': '. $message );
 }
