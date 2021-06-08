6690903a5d357d14ef724613a6246a961a2e3d0d
Marius Mandal <mariusmandal@gmail.com>
2021-06-08 23:21:42 +0200

    Ryddet opp i header-bruk

--- jQupload_cors.php
+++ jQupload_cors.php
@@ -1,15 +1,9 @@
 <?php
 
 require_once('UKMconfig.inc.php');
-
-################################################
-## SET ALL HEADERS AND ACTUALLY PERFORM UPLOAD 
-header('Access-Control-Allow-Headers: true');
-header('Access-Control-Allow-Origin: https://' . UKM_HOSTNAME);
-header('Access-Control-Request-Method: OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Credentials: true');
-//require('UploadHandler.php');
+require_once('inc/headers.inc.php');
 require_once('inc/jQupload_handler.inc.php');
+
 $upload_handler = new UploadHandler();
 die($upload_handler->get_body());
 ?>
